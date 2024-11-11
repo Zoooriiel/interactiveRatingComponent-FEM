adaee9862f5ba69f59ce0d0b59031e43e400e558
Gina Lim <zoooriiel@gmail.com>
2024-11-12 01:02:49 +0800

    First commit to save

---
+++ app.js
@@ -0,0 +1,13 @@
+const ratingButtons = document.querySelectorAll(".button")
+
+ratingButtons.forEach(button => {
+    button.addEventListener("click", () =>{
+        const rating = button.getAttribute('data-value');
+
+        if(rating === '1'){
+            
+        }
+    })
+
+    })
+
